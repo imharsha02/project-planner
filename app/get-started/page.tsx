453aa73c1f7f41fd01f5c5de5db4c56b729d4c8a
Harshavardhana Krishna Chaitanya <kccodes365@gmail.com>
2025-05-18 00:17:02 +0530

    Added express server and supabase links (url and anon key)

--- app/get-started/page.tsx
+++ app/get-started/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { TypographyH3 } from "../components/Typography/TypographyH3";
 
 const GetStarted = () => {
   return (
     <div>
-      
+      <TypographyH3 className="text-center">
+        Register to the planner
+      </TypographyH3>
+      <div>
+        
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GetStarted
+export default GetStarted;
